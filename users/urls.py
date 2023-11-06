38b8492e70f0b3db45f76ba369bce1142c638a47
ghost <kirill.han011@gmail.com>
2023-11-06 11:47:10 +0300

    added view company function

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import view_login, view_profile, view_logout, register_user
+from .views import (view_login, view_profile, view_logout, 
+                    register_user, view_company)
 
 app_name = 'users'
 
@@ -7,6 +8,6 @@ urlpatterns = [
     path('login/', view_login, name='login'),
     path('profile/', view_profile, name='profile'),
     path('logout/', view_logout, name='logout'),
-    path('register/', register_user, name='register_user')
+    path('register/', register_user, name='register_user'),
+    path('company/<int:user_id>/', view_company, name = 'company')
 ]
-
