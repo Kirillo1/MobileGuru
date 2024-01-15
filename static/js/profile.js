7bdcdbbd54f77424f5f59ad0c8be278cb5410515
ghost <kirill.han011@gmail.com>
2024-01-15 09:27:40 +0300

    fixed deleting profile image.

--- static/js/profile.js
+++ static/js/profile.js
@@ -10,34 +10,31 @@ function deleteImage() {
     image.style.display = "none";
 
     document.getElementById("image-clear_id").checked = true;
-    var imageContainer = document.getElementById('imageContainer');
-    var imgElement = document.createElement('img');
-    var pathToImage = "{% static 'default_avatar.png' %}";
-    imgElement.src = pathToImage;
+    const imageContainer = document.getElementById('imageContainer');
+    const imgElement = document.createElement('img');
+    imgElement.src = "/static/img/default_avatar.png";
     imgElement.className = "rounded-circle mt-5";
     imgElement.style.width = "200px";
     imageContainer.innerHTML = '';
     imageContainer.appendChild(imgElement);
     document.querySelector('#close_modal').click();
-
 }
 
-
 document.getElementById('id_image').addEventListener('change', function (event) {
-    var file = event.target.files[0];
+    const file = event.target.files[0];
 
     if (file) {
-        var reader = new FileReader();
+        const reader = new FileReader();
 
         reader.onload = function (e) {
-            var imageContainer = document.getElementById('imageContainer');
-            var imgElement = document.createElement('img');
+            const imageContainer = document.getElementById('imageContainer');
+            const imgElement = document.createElement('img');
             imgElement.src = e.target.result;
             imgElement.className = "rounded-circle mt-5";
             imgElement.style.width = "200px";
             imageContainer.innerHTML = '';
             imageContainer.appendChild(imgElement);
-            var image = document.getElementById('avatar_id');
+            const image = document.getElementById('avatar_id');
             image.style.display = "none";
             document.querySelector('#close_modal_chg').click();
         };
