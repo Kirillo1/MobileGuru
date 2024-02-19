50189944baa832d8bb2fe15cf016f0831f7a5c1b
ghost <kirill.han011@gmail.com>
2024-02-19 10:25:02 +0300

    Добавлена возможность оставлять комментарий для компании и смартфона

--- comments/urls.py
+++ comments/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import (create_comment_view, delete_comment_view, edit_comment_view)
+from .views import (create_comment_smarphone_view, delete_comment_view, edit_comment_view,
+                    create_comment_company_view)
 
 app_name = 'comments'
 
 urlpatterns = [
-    path('create_comment/<int:pk>', create_comment_view, name='create_comment'),
+    path('create_comment/<int:pk>', create_comment_smarphone_view, name='create_comment'),
+    path('create_comment_company/<str:title_hash>', create_comment_company_view, name='create_comment_company'),
     path('delete_comment/<int:pk>', delete_comment_view, name='delete_comment'),
     path('edit_comment/<int:pk>', edit_comment_view, name='edit_comment')
 ]
