47a34bc8b79cf2bcc2d53f83ab2e3f2c06937285
ghost <kirill.han011@gmail.com>
2024-02-21 23:53:58 +0300

    добавлена страница корзина

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
+from .views import view_basket
 
 app_name = 'orders'
 
 urlpatterns = [
-
+    path("basket/", view_basket, name="view_basket")
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,2 +1,4 @@
 from django.shortcuts import render
 
+def view_basket(request):
+    return render(request, 'orders/basket_detail.html')
