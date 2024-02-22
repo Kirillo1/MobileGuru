1b0e8c24447e5bbf0a5b65b20ec03da277ab2f38
ghost <kirill.han011@gmail.com>
2024-02-22 23:32:39 +0300

    добавлен ajax запрос для получения смартфона при заказе

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import view_basket
+from .views import view_basket, add_to_basket
 
 app_name = 'orders'
 
 urlpatterns = [
-    path("basket/", view_basket, name="view_basket")
+    path("basket/", view_basket, name="view_basket"),
+    path("add_to_basket/", add_to_basket, name="add_to_basket")
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.http import (Http404, HttpResponse, HttpResponseRedirect,
+                         JsonResponse)
 
 def view_basket(request):
     return render(request, 'orders/basket_detail.html')
 
+
+
+def add_to_basket(request):
+    if request.method != 'POST':
+        raise Http404
+
+    smartphone_id = request.POST.get('smartphone_id')
+    status = request.POST.get('status')
+    print(smartphone_id)
+        
+    return JsonResponse({'status': 'ok'})
