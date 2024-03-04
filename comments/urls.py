69868e43e1451ceb30e60351d0103b0fb7ff90b7
ghost <kirill.han011@gmail.com>
2024-03-04 11:53:02 +0300

    реализовано добавление, удаление и изменение комментариев к компании и к телефону

--- comments/urls.py
+++ comments/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import (create_comment_smarphone_view, delete_comment_view, edit_comment_view,
-                    create_comment_company_view)
+                    create_comment_company_view, delete_comment_company_view)
 
 app_name = 'comments'
 
 urlpatterns = [
     path('create_comment/', create_comment_smarphone_view, name='create_comment'),
-    path('create_comment_company/<str:title_hash>', create_comment_company_view, name='create_comment_company'),
+    path('create_comment_company/', create_comment_company_view, name='create_comment_company'),
     path('delete_comment/', delete_comment_view, name='delete_comment'),
-    path('edit_comment/<int:pk>', edit_comment_view, name='edit_comment')
+    path('delete_comment_company/<str:title_hash>/', delete_comment_company_view, name='delete_comment_company'),
+    path('edit_comment/<int:pk>/', edit_comment_view, name='edit_comment')
 ]
